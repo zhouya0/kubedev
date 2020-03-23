0e49219c077df8a865fc2a360f94d21cabd9c454
zhouya0 <yao.zhoupolytech@gmail.com>
2020-03-23 10:11:29 +0800

    Full the get version functions

--- pkg/env/kube_version.go
+++ pkg/env/kube_version.go
@@ -26,6 +26,8 @@ import (
 // cmd := exec.Command("cat", "build/build-image/cross/VERSION")
 // out, _ := cmd.CombinedOutput()
 
+var cleanTreeState string = "clean"
+
 type KubeVersion struct {
 	KubeGitCommit    string
 	KubeGitTreeState string
@@ -34,19 +36,27 @@ type KubeVersion struct {
 	KubeGitMinor     string
 }
 
-func (k *KubeVersion) getKubeGitCommit() error {
+func (k *KubeVersion) setKubeGitCommit(s string) error {
+	if s != "" {
+		k.KubeGitCommit = s
+		return nil
+	}
 	cmd := exec.Command("git", "rev-parse", "--short", "HEAD")
 	out, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Println(err.Error())
 		return err
 	}
-	log.Printf("Git commit is: %v", string(out))
 	k.KubeGitCommit = string(out)
+	log.Printf("Git commit is: %v \n", k.KubeGitCommit)
 	return nil
 }
 
-func (k *KubeVersion) getKubeGitVersion() error {
+func (k *KubeVersion) setKubeGitVersion(s string) error {
+	if s != "" {
+		k.KubeGitVersion = s
+		return nil
+	}
 	if k.KubeGitCommit == "" {
 		return fmt.Errorf("Can't get kube git version!")
 	}
@@ -57,14 +67,39 @@ func (k *KubeVersion) getKubeGitVersion() error {
 		return err
 	}
 	cutVersion := strings.Split(string(fullVersion), "-")
-	log.Printf("Kube version is: %v", cutVersion)
 	k.KubeGitVersion = cutVersion[0] + cutVersion[1]
+	log.Printf("Kube version is: %v \n", k.KubeGitVersion)
+	return nil
+}
+
+func (k *KubeVersion) setKubeTreeState(s string) error {
+	if s != "" {
+		k.KubeGitTreeState = s
+		return nil
+	}
+	k.KubeGitTreeState = cleanTreeState
+	log.Printf("Git tree state is: %v \n", k.KubeGitTreeState)
+	return nil
+}
+
+func (k *KubeVersion) setKubeGitMajorAndMinor() error {
+	if k.KubeGitVersion == "" {
+		return fmt.Errorf("Can't get kube major and minor version!")
+	}
+	versions := strings.Split(k.KubeGitVersion, ".")
+
+	k.KubeGitMajor = strings.Split(versions[0], "v")[1]
+	k.KubeGitMinor = versions[1]
+	log.Printf("Kube git major is: %v \n", k.KubeGitMajor)
+	log.Printf("Kube git minor is: %v \n", k.KubeGitMinor)
 	return nil
 }
 
 func NewKubeVersion() KubeVersion {
 	k := KubeVersion{}
-	k.getKubeGitCommit()
-	k.getKubeGitVersion()
+	k.setKubeGitCommit("")
+	k.setKubeGitVersion("")
+	k.setKubeTreeState("")
+	k.setKubeGitMajorAndMinor()
 	return k
 }
